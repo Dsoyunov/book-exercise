ffe985315550dfa413a147fce37515e6a0d7204e
Dsoyunov <44001734+Dsoyunov@users.noreply.github.com>
2019-02-04 11:53:35 -0500

    Added accessor for author

--- Book.java
+++ Book.java
@@ -3,8 +3,8 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author (Didar Soyunov.)
+ * @version (Feb 4,2019.)
  */
 class Book
 {
@@ -22,5 +22,12 @@ class Book
         title = bookTitle;
     }
 
-    // Add the methods here ...
-}
+    /**
+     * Provide access to the string holding the author
+     */
+    public String getauthor()
+        {
+           return author;
+        
+       }
+    }
